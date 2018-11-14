a00d23bb2e3400014f79217bc4d2f3bd1ba55356
Nadine <obwallern@gmail.com>
2018-11-14 21:41:48 +0100

    ex9 finished

--- ex9/Assignment8Tests.py
+++ ex9/Assignment8Tests.py
@@ -10,36 +10,38 @@ class Assignment8Tests(unittest.TestCase):
 	#--------------- test invalid strings ---------
 	def test_invalid_string_numbers(self):
 		string = "stringwithnumbers123"
-		self.assertRaises(ValueError, RLEString.RLEString, string)
+		self.assertRaises(Exception, RLEString.RLEString, string)
 
 	def test_invalid_string_special_chars(self):
 		string = "specialChars_!"
-		self.assertRaises(ValueError, RLEString.RLEString, string)
+		self.assertRaises(Exception, RLEString.RLEString, string)
 
 	# -------------- test compress -----------------
 	def test_already_compressed(self):
 		rle = RLEString.RLEString("Hello")
 		rle.compress()
-		with self.assertRaises(RuntimeError):
+		with self.assertRaises(Exception):
 			rle.compress()
 
 	def test_compressed_correctly(self):
 		rle = RLEString.RLEString("Hello")
 		hello_compressed = "1H1e2l1o"
-		self.assertEqual(rle.compress(), hello_compressed)
+		rle.compress()
+		self.assertEqual(rle.__str__(), hello_compressed)
 
 	# -------------- test decompress -----------------
 	def test_already_decompressed(self):
 		rle = RLEString.RLEString("Hello")
 		#print(rle.compress())
-		with self.assertRaises(RuntimeError):
+		with self.assertRaises(Exception):
 			rle.decompress()
 
 	def test_decompressed_correctly(self):
 		rle = RLEString.RLEString("Hello")
 		hello_decompressed = "Hello"
 		rle.compress()
-		self.assertEqual(rle.decompress(), hello_decompressed)
+		rle.decompress()
+		self.assertEqual(rle.__str__(), hello_decompressed)
 
 	# --------------- test iscompressed ---------------
 	def test_iscompressed_true(self):
